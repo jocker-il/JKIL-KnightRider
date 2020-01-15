1cd743f7e9700bf57a99c6bf4aebc94248f50560
The JoCKeR (IL) <13031901+jocker-il@users.noreply.github.com>
2020-01-15 15:44:48 +0100

    Ver 1.1.1 - Fixes and closes issue #6
    # Bug fix and code rework
    
      Fix:
        - Fixed game crash when driving and car is destroyed
        - code beautification to make it faster and better (not that it was a problem before)

--- control.lua
+++ control.lua
@@ -1,17 +1,13 @@
 red = {r = 1, a = 0.5}
 script.on_event(defines.events.on_player_driving_changed_state,
-				function(event)
-					local first_player = game.players[event.player_index]
-					local vehicle = event.entity.type
-					if not first_player.driving then
-						return
---					elseif not first_player.driving and vehicle == "car" then
-					elseif vehicle == "car" then
-						first_player.print("Hello Michael",red)
-						first_player.play_sound{path="KITT"}
-					end
-				end
-			)
+	function(event)
+		local player = game.players[event.player_index]
+		if player.driving and event.entity and event.entity.type == "car" then
+			player.print("Hello Michael",red)
+			player.play_sound{path="KITT"}
+		end
+	end
+	)
 
 
 
